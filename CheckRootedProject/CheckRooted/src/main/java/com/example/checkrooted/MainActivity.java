264e80ad99acf47b2d406ee74db2dc7247e688c0
GT <gt@new-host-6.ca.cewit.int>
2013-06-07 12:26:32 -0400

    Pushing the project to the private repo

---
+++ CheckRootedProject/CheckRooted/src/main/java/com/example/checkrooted/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.checkrooted;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.File;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView t = (TextView) findViewById(R.id.textViewSimple);
+
+        t.setText(" iS Device Rooted : " + Boolean.toString(isDeviceRooted()));
+    }
+
+    public static boolean isDeviceRooted() {
+
+        // get from build info
+        String buildTags = android.os.Build.TAGS;
+        if (buildTags != null && buildTags.contains("test-keys")) {
+            return true;
+        }
+
+        // check if /system/app/Superuser.apk is present
+        try {
+            File file = new File("/system/app/Superuser.apk");
+            if (file.exists()) {
+                return true;
+            }
+        } catch (Throwable e1) {
+            // ignore
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
